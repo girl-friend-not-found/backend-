ea1e54ad6b187c29132dd54eb152e0c50b114ddc
tofu <66718374+toufu-cell@users.noreply.github.com>
2025-02-12 13:13:42 +0900

    config delete

--- config.example.py
+++ config.example.py
@@ -1,9 +1,8 @@
-# config.py
 import os
 
 class Settings:
     # 環境変数から取得、なければデフォルト値
-    OPENAI_API_KEY: str = os.environ.get("OPENAI_API_KEY", "openai_api_key")
+    OPENAI_API_KEY: str = os.environ.get("OPENAI_API_KEY", "your-api-key-here")
     OPENAI_MODEL_NAME: str = os.environ.get("OPENAI_MODEL_NAME", "gpt-4-0125-preview")
 
-settings = Settings()
+settings = Settings() 

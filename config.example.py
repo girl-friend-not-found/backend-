2a8c6beb3a8bdede93190b3b005b3dbdd9f5e088
tofu <66718374+toufu-cell@users.noreply.github.com>
2025-02-13 23:05:53 +0900

    Update config.example.py

--- config.example.py
+++ config.example.py
@@ -3,6 +3,6 @@
 #class Settings:
     # 環境変数から取得、なければデフォルト値
     #OPENAI_API_KEY: str = os.environ.get("OPENAI_API_KEY", "your-api-key-here")
-    #OPENAI_MODEL_NAME: str = os.environ.get("OPENAI_MODEL_NAME", "gpt-4-0125-preview")
+    #OPENAI_MODEL_NAME: str = os.environ.get("OPENAI_MODEL_NAME", "gpt-3.5-turbo")
 
 #settings = Settings() 
